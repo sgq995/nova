041255ec797a9d59bc8082dd0aebf310ce64d23a
Sebastian G. Q. <sgq995@gmail.com>
2025-02-26 20:30:32 -0500

    feat: bundle html templates

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require github.com/evanw/esbuild v0.25.0
 require (
 	github.com/tdewolff/minify/v2 v2.21.3 // indirect
 	github.com/tdewolff/parse/v2 v2.7.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
--- internal/bundler/bundler.go
+++ internal/bundler/bundler.go
@@ -1,14 +1,22 @@
 package bundler
 
 import (
+	"bytes"
 	_ "embed"
+	"encoding/json"
+	"errors"
 	"io/fs"
 	"os"
+	"path"
 	"path/filepath"
+	"slices"
+	"strings"
 
 	"github.com/evanw/esbuild/pkg/api"
 	"github.com/tdewolff/minify/v2"
-	"github.com/tdewolff/minify/v2/html"
+	minifyHtml "github.com/tdewolff/minify/v2/html"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 	"segoqu.com/nova/internal/project"
 )
 
@@ -40,6 +48,18 @@ func findFiles(dir string, validExtensions map[string]bool) ([]string, error) {
 	return files, err
 }
 
+func cleanDir(dir string) error {
+	err := os.RemoveAll(dir)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func Development(dir string) (api.BuildContext, error) {
 	// TODO: run on init
 	hmrResult := api.Build(api.BuildOptions{
@@ -65,7 +85,7 @@ func Development(dir string) (api.BuildContext, error) {
 		".tsx": true,
 		".css": true,
 	}
-	entries, err := findFiles("src/pages", validExtensions)
+	entries, err := findFiles(dir, validExtensions)
 	if err != nil {
 		return nil, err
 	}
@@ -89,34 +109,75 @@ func Development(dir string) (api.BuildContext, error) {
 	return ctx, nil
 }
 
-func Production(dir string) (api.BuildContext, error) {
-	validExtensions := map[string]bool{
-		".js":   true,
-		".jsx":  true,
-		".ts":   true,
-		".tsx":  true,
-		".css":  true,
-		".html": true,
+type buildContext struct {
+	static    api.BuildContext
+	templates api.BuildContext
+}
+
+func (ctx *buildContext) Cancel() {
+	ctx.static.Cancel()
+	ctx.templates.Cancel()
+}
+
+func (ctx *buildContext) Dispose() {
+	ctx.static.Dispose()
+	ctx.templates.Dispose()
+}
+
+func (ctx *buildContext) Rebuild() api.BuildResult {
+	r1 := ctx.static.Rebuild()
+	r2 := ctx.templates.Rebuild()
+	return api.BuildResult{
+		Errors:      slices.Concat(r1.Errors, r2.Errors),
+		Warnings:    slices.Concat(r1.Warnings, r2.Warnings),
+		Metafile:    r1.Metafile,
+		OutputFiles: slices.Concat(r1.OutputFiles, r2.OutputFiles),
+		MangleCache: r1.MangleCache,
 	}
-	entries, err := findFiles("src/pages", validExtensions)
+}
+
+func (ctx *buildContext) Serve(api.ServeOptions) (api.ServeResult, error) {
+	return api.ServeResult{}, errors.ErrUnsupported
+}
+
+func (ctx *buildContext) Watch(api.WatchOptions) error {
+	return errors.ErrUnsupported
+}
+
+func Production(dir string) (api.BuildContext, error) {
+	static, err := findFiles(dir, map[string]bool{
+		".js":  true,
+		".jsx": true,
+		".ts":  true,
+		".tsx": true,
+		".css": true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	templates, err := findFiles(dir, map[string]bool{
+		".html": true,
+	})
 	if err != nil {
 		return nil, err
 	}
 
 	outDir := project.Abs(".nova")
 	staticDir := project.Abs(filepath.Join(".nova", "static"))
-	err = os.RemoveAll(staticDir)
+	err = cleanDir(staticDir)
 	if err != nil {
 		return nil, err
 	}
-	err = os.MkdirAll(staticDir, 0755)
+	templatesDir := project.Abs(filepath.Join(".nova", "templates"))
+	err = cleanDir(templatesDir)
 	if err != nil {
 		return nil, err
 	}
 
 	// TODO: independent context for html files only to save html files without hash
-	ctx, ctxErr := api.Context(api.BuildOptions{
-		EntryPoints: entries,
+	staticCtx, ctxErr := api.Context(api.BuildOptions{
+		EntryPoints: static,
 
 		EntryNames: "[dir]/[name].[hash]",
 
@@ -146,6 +207,24 @@ func Production(dir string) (api.BuildContext, error) {
 					})
 				},
 			},
+		},
+	})
+	if ctxErr != nil {
+		return nil, ctxErr
+	}
+
+	tempaltesCtx, ctxErr := api.Context(api.BuildOptions{
+		EntryPoints: templates,
+
+		EntryNames: "[dir]/[name]",
+
+		Write: true,
+
+		Outdir: templatesDir,
+
+		Sourcemap: api.SourceMapNone,
+
+		Plugins: []api.Plugin{
 			{
 				Name: "nova-html",
 				Setup: func(pb api.PluginBuild) {
@@ -155,15 +234,81 @@ func Production(dir string) (api.BuildContext, error) {
 							return api.OnLoadResult{}, err
 						}
 
-						contents := string(b)
-
-						m := minify.New()
-						m.AddFunc("text/html", html.Minify)
-						contents, err = m.String("text/html", contents)
+						metafile, err := os.ReadFile(filepath.Join(outDir, "meta.json"))
 						if err != nil {
 							return api.OnLoadResult{}, err
 						}
 
+						meta := make(map[string]any)
+						err = json.Unmarshal(metafile, &meta)
+						if err != nil {
+							return api.OnLoadResult{}, err
+						}
+
+						srcMap := make(map[string]string)
+						outputs := meta["outputs"].(map[string]any)
+						// outPrefix := path.Join(".nova", "static")
+						outPrefix := ".nova"
+						inPrefix := filepath.ToSlash(dir)
+						for out := range outputs {
+							val := outputs[out].(map[string]any)
+							in := val["entryPoint"].(string)
+
+							outSrc := strings.TrimPrefix(out, outPrefix)
+							inSrc := strings.TrimPrefix(in, inPrefix)
+
+							srcMap[inSrc] = outSrc
+						}
+
+						currentDir := strings.TrimPrefix(ola.Path, filepath.Join(project.Root(), dir))
+						currentDir = filepath.Dir(currentDir)
+						currentDir = filepath.ToSlash(currentDir)
+
+						doc, err := html.Parse(bytes.NewReader(b))
+						mapFiles := func(n *html.Node) {
+							if n.Type != html.ElementNode {
+								return
+							}
+
+							if n.DataAtom != atom.Script && n.DataAtom != atom.Link {
+								return
+							}
+
+							target := "src"
+							if n.DataAtom == atom.Link {
+								target = "href"
+							}
+
+							for i, attr := range n.Attr {
+								if attr.Key != target {
+									continue
+								}
+
+								in := path.Clean(attr.Val)
+								in = path.Join(currentDir, in)
+
+								if hashed, ok := srcMap[in]; ok {
+									newSrc := hashed
+									n.Attr[i].Val = newSrc
+								}
+							}
+						}
+
+						for n := range doc.Descendants() {
+							mapFiles(n)
+						}
+
+						var buf bytes.Buffer
+						html.Render(&buf, doc)
+
+						m := minify.New()
+						m.Add("text/html", &minifyHtml.Minifier{TemplateDelims: minifyHtml.GoTemplateDelims})
+						b, err = m.Bytes("text/html", buf.Bytes())
+						if err != nil {
+							return api.OnLoadResult{}, err
+						}
+
+						contents := string(b)
 						return api.OnLoadResult{
 							Contents: &contents,
 							Loader:   api.LoaderCopy,
@@ -182,5 +327,5 @@ func Production(dir string) (api.BuildContext, error) {
 		return nil, ctxErr
 	}
 
-	return ctx, nil
+	return &buildContext{static: staticCtx, templates: tempaltesCtx}, nil
 }
