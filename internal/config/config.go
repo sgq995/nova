af45b5cbfa11df03c9271dfa5d9e66d063ea3853
Sebastian G. Q. <sgq995@gmail.com>
2025-02-28 15:08:38 -0500

    feat: config module

---
+++ internal/config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	Pages string // Route to pages dir
+}
+
+func defaultConfig() Config {
+	return Config{
+		Pages: "src/pages",
+	}
+}
+
+type configFile struct {
+	Pages *string
+}
+
+func transformConfigFile(f *configFile) Config {
+	pages := ""
+	if f.Pages != nil {
+		pages = *f.Pages
+	}
+
+	return Config{
+		Pages: pages,
+	}
+}
+
+func readConfigFile(filename string) (Config, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var f configFile
+	buf := bytes.NewReader(b)
+	json.NewDecoder(buf).Decode(&f)
+
+	config := transformConfigFile(&f)
+
+	return config, nil
+}
+
+func override(base Config, config Config) Config {
+	var final Config = base
+
+	if config.Pages != "" {
+		final.Pages = config.Pages
+	}
+
+	return final
+}
+
+func Read(filename string) (Config, error) {
+	def := defaultConfig()
+
+	user, err := readConfigFile(filename)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config := override(def, user)
+	return config, nil
+}
