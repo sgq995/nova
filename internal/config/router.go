41ffeeb4714d80e9d0331fa6ebacddad5a0041c4
Sebastian G. Q. <sgq995@gmail.com>
2025-03-04 18:40:00 -0500

    feat(config): api base path

--- internal/config/router.go
+++ internal/config/router.go
@@ -1,12 +1,14 @@
 package config
 
 type RouterConfig struct {
-	Pages string // relative path to pages dir, it defaults to "src/pages"
+	Pages   string // relative path to pages dir, it defaults to "src/pages"
+	APIBase string // base path for rest routes, it defaults to "/api"
 }
 
 func defaultRouterConfig() RouterConfig {
 	return RouterConfig{
-		Pages: "src/pages",
+		Pages:   "src/pages",
+		APIBase: "/api",
 	}
 }
 
@@ -14,14 +16,30 @@ func (rc *RouterConfig) merge(other *RouterConfig) {
 	if other.Pages != "" {
 		rc.Pages = other.Pages
 	}
+
+	if other.APIBase != "" {
+		rc.APIBase = other.APIBase
+	}
 }
 
 type routerConfigFile struct {
-	Pages string `json:"pages"`
+	Pages   *string `json:"pages"`
+	APIBase *string `json:"apiBase"`
 }
 
 func transformRouterConfigFile(rcf *routerConfigFile) RouterConfig {
+	var pages string
+	if rcf.Pages != nil {
+		pages = *rcf.Pages
+	}
+
+	var apiBase string
+	if rcf.APIBase != nil {
+		apiBase = *rcf.APIBase
+	}
+
 	return RouterConfig{
-		Pages: rcf.Pages,
+		Pages:   pages,
+		APIBase: apiBase,
 	}
 }
