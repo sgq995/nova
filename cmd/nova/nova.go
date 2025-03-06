723d91240a659cf17aabdf4bbd96c025208a01fc
Sebastian G. Q. <sgq995@gmail.com>
2025-03-06 09:52:13 -0500

    feat(cmd): new interface

--- cmd/nova/nova.go
+++ cmd/nova/nova.go
@@ -10,111 +10,44 @@ import (
 	"os/exec"
 	"os/signal"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"syscall"
 
-	"github.com/evanw/esbuild/pkg/api"
 	"github.com/sgq995/nova/internal/bundler"
+	"github.com/sgq995/nova/internal/config"
 	"github.com/sgq995/nova/internal/generator"
 	"github.com/sgq995/nova/internal/module"
+	"github.com/sgq995/nova/internal/project"
+	"github.com/sgq995/nova/internal/utils"
 )
 
-func must[T any](obj T, err error) T {
-	if err != nil {
-		panic(err)
-	}
-	return obj
-}
-
-func dev() {
-	command := func(ctx context.Context) *exec.Cmd {
-		cmd := exec.CommandContext(ctx, "go", "run", module.Abs(filepath.Join(".nova", "main.go")))
-		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		return cmd
-	}
-
-	start := func(cmd *exec.Cmd) {
-		err := cmd.Start()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		log.Println("[server] started http://localhost:8080")
-	}
-
-	stop := func(cmd *exec.Cmd) {
-		cmd.Process.Signal(os.Interrupt)
-		syscall.Kill(-cmd.Process.Pid, syscall.SIGINT)
-		cmd.Wait()
-
-		cmd.Process.Kill()
-		syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
-		log.Println("[server] stoped")
-	}
-
-	generate := func(addr string) {
-		routes := must(generator.FindRoutes("src/pages"))
-		err := generator.GenerateServer(routes, generator.OptionProxy(addr), generator.OptionEnvironment(generator.Dev))
-		if err != nil {
-			panic(err)
-		}
-	}
+func dev(c config.Config) {
+	nova, _ := project.Context(c)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	cmd := command(ctx)
-
-	esbuild := must(bundler.Development("src/pages"))
-	defer esbuild.Dispose()
+	server, err := nova.Serve(ctx)
+	if err != nil {
+		log.Fatalln("fatal", err)
+	}
+	defer server.Dispose()
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGABRT, syscall.SIGTERM, syscall.SIGKILL)
 
 	go func() {
 		<-sig
-		stop(cmd)
-		esbuild.Dispose()
+		server.Dispose()
 		cancel()
 	}()
 
-	err := esbuild.Watch(api.WatchOptions{})
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	serve := must(esbuild.Serve(api.ServeOptions{Host: "127.0.0.1", Port: 0}))
-	for _, host := range serve.Hosts {
-		log.Println(
-			"[esbuild]", "bundler server listening at",
-			host+":"+strconv.Itoa(int(serve.Port)),
-		)
-	}
-
-	addr := serve.Hosts[0] + ":" + strconv.Itoa(int(serve.Port))
-
-	generate(addr)
-	start(cmd)
-
-	watcher := generator.NewWatcher(ctx, "*.go", func(s string) {
-		log.Println("[reload]", s)
-
-		stop(cmd)
-		cmd = command(ctx)
-
-		generate(addr)
-		start(cmd)
-	})
-	go watcher.Watch("src/pages")
-
+	log.Printf("[nova] http://%s:%d\n", c.Server.Host, c.Server.Port)
 	<-ctx.Done()
 }
 
 func build() {
-	esbuild := must(bundler.Production("src/pages"))
+	esbuild := utils.Must(bundler.Production("src/pages"))
 	defer esbuild.Dispose()
 
 	result := esbuild.Rebuild()
@@ -129,7 +62,7 @@ func build() {
 		log.Println(strings.TrimPrefix(file.Path, module.Root()+"/"))
 	}
 
-	routes := must(generator.FindRoutes("src/pages"))
+	routes := utils.Must(generator.FindRoutes("src/pages"))
 	err := generator.GenerateServer(routes, generator.OptionStatic(), generator.OptionEnvironment(generator.Prod))
 	if err != nil {
 		panic(err)
@@ -153,31 +86,37 @@ func build() {
 
 func help() {
 	flag.Usage()
-	fmt.Fprintf(flag.CommandLine.Output(), "\t%s %s\n", os.Args[0], "dev|build")
+	fmt.Fprintf(flag.CommandLine.Output(), "\n%s %s\n", os.Args[0], "dev|build")
 }
 
 func main() {
-	// configFileSet := flag.NewFlagSet("config-file", flag.ContinueOnError)
-	// configFile := configFileSet.String(
-	// 	"config-file",
-	// 	filepath.Join(module.Root(), "nova.config.json"),
-	// 	"A JSON config file",
-	// )
-	// configFileSet.Parse(os.Args)
-
-	devCmd := flag.NewFlagSet("dev", flag.ExitOnError)
+	configFile := flag.String(
+		"config-file",
+		filepath.Join(module.Root(), "nova.config.json"),
+		"A JSON config file",
+	)
 
 	flag.Parse()
 
-	if len(os.Args) < 2 {
-		help()
-		os.Exit(1)
+	c := config.Default()
+	if *configFile != "" {
+		c2, err := config.Read(*configFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		c = c2
 	}
 
-	switch os.Args[1] {
+	args := flag.Args()
+	if len(args) < 1 {
+		help()
+		return
+	}
+
+	switch args[0] {
 	case "dev":
-		devCmd.Parse(os.Args[2:])
-		dev()
+		dev(c)
 
 	case "build":
 		build()
