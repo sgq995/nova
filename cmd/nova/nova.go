f930fbe8ad2926430d08c586c35d6349a6708d7f
Sebastian G. Q. <sgq995@gmail.com>
2025-03-07 21:41:25 -0500

    feat(cmd): build command

--- cmd/nova/nova.go
+++ cmd/nova/nova.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -10,19 +9,16 @@ import (
 	"os/exec"
 	"os/signal"
 	"path/filepath"
-	"strings"
 	"syscall"
 
-	"github.com/sgq995/nova/internal/bundler"
 	"github.com/sgq995/nova/internal/config"
-	"github.com/sgq995/nova/internal/generator"
 	"github.com/sgq995/nova/internal/module"
 	"github.com/sgq995/nova/internal/project"
 	"github.com/sgq995/nova/internal/utils"
 )
 
 func dev(c config.Config) {
-	nova, _ := project.Context(c)
+	nova := utils.Must(project.Context(c))
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -46,31 +42,16 @@ func dev(c config.Config) {
 	<-ctx.Done()
 }
 
-func build() {
-	esbuild := utils.Must(bundler.Production("src/pages"))
-	defer esbuild.Dispose()
-
-	result := esbuild.Rebuild()
-	if len(result.Errors) > 0 {
-		errs := []error{}
-		for _, e := range result.Errors {
-			errs = append(errs, errors.New(e.Text))
-		}
-		panic(errors.Join(errs...))
-	}
-	for _, file := range result.OutputFiles {
-		log.Println(strings.TrimPrefix(file.Path, module.Root()+"/"))
-	}
-
-	routes := utils.Must(generator.FindRoutes("src/pages"))
-	err := generator.GenerateServer(routes, generator.OptionStatic(), generator.OptionEnvironment(generator.Prod))
+func build(c config.Config) {
+	nova := utils.Must(project.Context(c))
+	err := nova.Build()
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
 	log.Println("[build] go build -o .nova/app")
-	in := filepath.Join(module.Root(), ".nova", "main.go")
-	out := filepath.Join(module.Root(), ".nova", "app")
+	in := filepath.Join(module.Root(), c.Codegen.OutDir, "main.go")
+	out := filepath.Join(module.Root(), c.Codegen.OutDir, "app")
 	cmd := exec.Command("go", "build", "-o", out, in)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -119,7 +100,7 @@ func main() {
 		dev(c)
 
 	case "build":
-		build()
+		build(c)
 
 	default:
 		help()
