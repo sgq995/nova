9f4bea571534df40d728bc8dba559f4516071510
Sebastian G. Q. <sgq995@gmail.com>
2025-03-17 18:48:46 -0500

    refactor(watcher): event dispatch

--- internal/watcher/callback.go
+++ internal/watcher/callback.go
@@ -15,19 +15,42 @@ type Callback struct {
 	OnDelete CallbackFunc
 }
 
-type CallbackMap map[string]Callback
+type eventCallbackMap map[string]CallbackFunc
 
-// TODO: split callbacks by groups: create, update, delete and filter out nil callbacks
-func (cm CallbackMap) match(filename string) []Callback {
+func (ecm eventCallbackMap) match(filename string) []CallbackFunc {
 	name := filepath.Base(filename)
-	matches := []Callback{}
-	for matcher := range cm {
+	matches := []CallbackFunc{}
+	for matcher, cb := range ecm {
 		patterns := strings.Split(matcher, ",")
 		for _, pattern := range patterns {
 			if matched := utils.Must(filepath.Match(pattern, name)); matched {
-				matches = append(matches, cm[matcher])
+				matches = append(matches, cb)
 			}
 		}
 	}
 	return matches
+}
+
+type CallbackMap map[string]Callback
+
+func (cm CallbackMap) splitEventCallbacks() (eventCallbackMap, eventCallbackMap, eventCallbackMap) {
+	created := eventCallbackMap{}
+	updated := eventCallbackMap{}
+	deleted := eventCallbackMap{}
+
+	for pattern, cb := range cm {
+		if cb.OnCreate != nil {
+			created[pattern] = cb.OnCreate
+		}
+
+		if cb.OnUpdate != nil {
+			updated[pattern] = cb.OnUpdate
+		}
+
+		if cb.OnDelete != nil {
+			deleted[pattern] = cb.OnDelete
+		}
+	}
+
+	return created, updated, deleted
 }
--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -9,41 +9,28 @@ import (
 	"github.com/sgq995/nova/internal/module"
 )
 
-func dispatchFunc(files []string, match func(string) []Callback, execute func(cb Callback, filename string) error) {
+func dispatchFunc(files []string, ecm eventCallbackMap) {
 	for _, filename := range files {
-		matches := match(filename)
+		matches := ecm.match(filename)
 		if len(matches) == 0 {
 			continue
 		}
 
 		for _, cb := range matches {
 			// TODO: verify if WaitGroup is needed
-			go execute(cb, filename)
+			go cb(filename)
 		}
 	}
 }
 
 func dispatch(files *fileEvents, callbacks CallbackMap) {
-	dispatchFunc(files.created, callbacks.match, func(cb Callback, filename string) error {
-		if cb.OnCreate == nil {
-			return nil
-		}
-		return cb.OnCreate(filename)
-	})
+	created, updated, deleted := callbacks.splitEventCallbacks()
 
-	dispatchFunc(files.updated, callbacks.match, func(cb Callback, filename string) error {
-		if cb.OnUpdate == nil {
-			return nil
-		}
-		return cb.OnUpdate(filename)
-	})
+	dispatchFunc(files.created, created)
 
-	dispatchFunc(files.deleted, callbacks.match, func(cb Callback, filename string) error {
-		if cb.OnDelete == nil {
-			return nil
-		}
-		return cb.OnDelete(filename)
-	})
+	dispatchFunc(files.updated, updated)
+
+	dispatchFunc(files.deleted, deleted)
 }
 
 func WatchDir(ctx context.Context, dir string, callbacks CallbackMap) error {
@@ -79,41 +66,4 @@ func WatchDir(ctx context.Context, dir string, callbacks CallbackMap) error {
 			time.Sleep(250 * time.Millisecond)
 		}
 	}
-
-	// err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	if d.IsDir() {
-	// 		return nil
-	// 	}
-
-	// 	matches := callbacks.match(path)
-	// 	if len(matches) == 0 {
-	// 		return nil
-	// 	}
-
-	// 	info, err := d.Info()
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	modTime := info.ModTime()
-	// 	lastModTime, exists := fileInfo[path]
-	// 	switch {
-	// 	case exists && modTime.After(lastModTime):
-	// 		for _, cb := range matches {
-	// 			cb.OnUpdate(path)
-	// 		}
-
-	// 	case !exists:
-	// 		for _, cb := range matches {
-	// 			cb.OnCreate(path)
-	// 		}
-	// 	}
-	// 	fileInfo[path] = modTime
-
-	// 	return nil
-	// })
 }
